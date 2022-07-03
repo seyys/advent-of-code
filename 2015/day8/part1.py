126544ca97d009cfe95629bade85e2a0b9ed0c7a
Daniel Yang <danielysyang@gmail.com>
2022-07-04 00:13:52 +1200

    2015 Day 8

---
+++ 2015/day8/part1.py
@@ -0,0 +1,23 @@
+import re
+
+with open('input.txt') as f:
+    d = f.readlines()
+    d = [x.strip() for x in d]
+
+mem_lit = 0
+mem_str = 0
+
+for row in d:
+    mem_l = len(row)
+
+    row = row[1:-1]
+
+    mem_s = 2
+    mem_s += len(re.findall(r"\\\\", row))
+    mem_s += len(re.findall(r"\\\"", row))
+    mem_s += len(re.findall(r"\\x[0-9a-f]{2}", row)) * 3
+
+    mem_lit += mem_l
+    mem_str += mem_l - mem_s
+
+print(mem_lit - mem_str)
