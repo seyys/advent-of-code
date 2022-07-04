7db6cf83575e8a9a2ac12964c701558b80419195
Daniel Yang <danielysyang@gmail.com>
2022-07-05 00:51:03 +1200

    2015 Day 9

---
+++ 2015/day9/part1.py
@@ -0,0 +1,30 @@
+import itertools
+import numpy as np
+import re
+
+with open('input.txt') as f:
+    d = f.readlines()
+    d = [x.strip() for x in d]
+    d = [re.split(r" to | = ",x) for x in d]
+
+dist = dict()
+cities = []
+shortest_path_len = np.inf
+
+for row in d:
+    if row[0] not in cities:
+        cities.append(row[0])
+        dist[row[0]] = dict()
+    if row[1] not in cities:
+        cities.append(row[1])
+        dist[row[1]] = dict()
+    dist[row[0]][row[1]] = int(row[2])
+    dist[row[1]][row[0]] = int(row[2])
+
+for path in itertools.permutations(cities):
+    path_len = 0
+    for i in range(len(path) - 1):
+        path_len += dist[path[i]][path[i+1]]
+    shortest_path_len = min(shortest_path_len, path_len)
+
+print(shortest_path_len)
