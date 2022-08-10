897d7ea54bcdc46acc5dcd5b06f027d705720204
Daniel Yang <danielysyang@gmail.com>
2022-08-10 23:14:37 +1200

    2016 day 6

---
+++ 2016/day6/part1.py
@@ -0,0 +1,16 @@
+import numpy as np
+from collections import Counter
+
+
+with open("input.txt") as f:
+    d = f.readlines()
+d = [x.strip() for x in d]
+d = [[x for x in row] for row in d]
+d = np.asarray(d)
+
+message = ""
+
+for col in d.T:
+    message += Counter(col).most_common(1)[0][0]
+    
+print(message)
