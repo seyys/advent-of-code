f59cdfa4f2991a5224616ce94f2d9907ea02424d
Daniel Yang <danielysyang@gmail.com>
2024-12-07 01:21:11 +1100

    2024 day 6 optimised

--- 2024/day6/part2.go
+++ 2024/day6/part2.go
@@ -3,6 +3,7 @@ package day6
 import (
 	"fmt"
 	"os"
+	"sync"
 )
 
 func Part2() {
@@ -29,22 +30,69 @@ func Part2() {
 		}
 	}
 
-	count := 0
-	for r, row := range grid {
-		for c := range row {
-			if grid[r][c] != '.' {
-				continue
+	// Narrow search space by only placing obstacles on the path that the guard patrols
+	r := startR
+	c := startC
+	dir := [2]int{-1, 0}
+	obstaclePositions := [][2]int{}
+	for {
+		if r+dir[0] < 0 || r+dir[0] >= len(grid) || c+dir[1] < 0 || c+dir[1] >= len(grid[0]) {
+			break
+		}
+		nextChar := grid[r+dir[0]][c+dir[1]]
+		if nextChar == '#' {
+			dir[0], dir[1] = dir[1], -dir[0]
+		} else {
+			if grid[r][c] != 'X' {
+				obstaclePositions = append(obstaclePositions, [2]int{r, c})
 			}
+			grid[r][c] = 'X'
+			r += dir[0]
+			c += dir[1]
+		}
+	}
+
+	// Count number of positions obstructions can be placed to make guard loop
+	count := 0
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+
+	// Channel to collect results from goroutines
+	results := make(chan int, len(grid)*len(grid[0])) // buffer size to avoid blocking
+
+	for _, obstaclePosition := range obstaclePositions {
+		r := obstaclePosition[0]
+		c := obstaclePosition[1]
+
+		// Increment the WaitGroup counter for each goroutine
+		wg.Add(1)
+
+		// Launch a goroutine for each valid iteration
+		go func(r, c int) {
+			defer wg.Done() // Decrement the WaitGroup counter when the goroutine is done
+
 			newGrid := make([][]byte, len(grid))
 			for i := range grid {
 				newGrid[i] = make([]byte, len(grid[i]))
 				copy(newGrid[i], grid[i])
 			}
 			newGrid[r][c] = '#'
+
 			if isLoop(startR, startC, newGrid) {
-				count++
+				mu.Lock()
+				results <- 1
+				mu.Unlock()
+			} else {
+				results <- 0
 			}
-		}
+		}(r, c) // Pass current r and c to the goroutine
+	}
+
+	wg.Wait()
+	close(results)
+
+	for result := range results {
+		count += result
 	}
 
 	fmt.Println(count)
